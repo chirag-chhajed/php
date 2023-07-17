7a5f62e49cc7c99243a8f7fe5f3e18c655a30ca4
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 19:35:31 +0530

    done

--- index.php
+++ index.php
@@ -1,36 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label for="">Username:</label>
+        <input type="text" name="username" id=""><br>
+        <label for="">Password</label>
+        <input type="password" name="password" id=""><br>
+        <input type="submit" value="Login" name="login">
+    </form>
+</body>
+
+</html>
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
-
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
-
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
-
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
+// isSet() function to check if the variable is set or not
+// empty() function to check if the variable is empty or not
+// foreach($_POST as $key => $value){
+//     echo "{$key} = {$value} <br>";
+// }
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if (empty($username)) {
+        echo "Username is missing <br>";
+    }elseif(empty($password)){
+        echo "Password is missing <br>";
+    }
+    else{
+        echo "Welcome {$username}";
+    }}
 ?>
