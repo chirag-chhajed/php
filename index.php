8bc2f6ad61324765c8808e56452c50f4d5a6d545
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-18 00:45:07 +0530

    done

--- index.php
+++ index.php
@@ -1,36 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <input type="text" name="username" placeholder="Enter you username" id="">
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+    // _SERVER - SGB that contains headers, path, and script slocations
+    // The entries in this array are created by the web server.
+    // Show nearly everything you need to know about the current web page env.
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        echo "Hello";
+    }
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
+    // foreach($_SERVER as $key => $value){
+    //     echo $key . " : " . $value . "<br>";
+    // }
 
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
 
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
 ?>
