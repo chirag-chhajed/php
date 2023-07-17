d26350efdb099a26126fb7b2e55810de0faf42f6
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 14:12:09 +0530

    arithmetic done

--- index.php
+++ index.php
@@ -1,36 +1,28 @@
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+    // Arithmetic operators
+    // + - * / ** %
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+    $x = 10;
+    $y = 2;
+    $z = null;
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
+    $z = $x - $y;
+    echo "{$z} <br>";
 
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
+    // Increment/Decrement operators
+    $counter = 0;
+    $counter+=3;
+    echo $counter;
+    echo "<br>";
 
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
+    // Operator Precedence
+    // PEMDAS
+    // ()
+    // **
+    // * / %
+    // + -
 
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
+    $total = 1+2-3*4/5**6;
+    echo $total;
 
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
 ?>
