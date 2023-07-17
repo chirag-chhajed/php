c4151cefc1d1e5889be4457b4b86ab75e8dd1202
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 19:57:28 +0530

    done

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="radio" name="credit_card" id="" value="visa">Visa <br>
+        <input type="radio" name="credit_card" id="" value="mastercard">Mastercard <br>
+        <input type="radio" name="credit_card" id="" value="AmericanExpress">American Express <br>
+        <input type="submit" value="confirm">
+    </form>
+
+    <form action="index.php" method="post">
+        <input type="checkbox" name="pizza" value="Pizza">Pizza<br>
+        <input type="checkbox" name="pasta" value="Pasta">Pasta<br>
+        <input type="checkbox" name="salad" value="Salad">Salad<br>
+        <input type="submit" value="submit">
+
+    </form>
+</body>
+
+</html>
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+// radio
+if (isset($_POST['credit_card'])) {
+    $credit_card = $_POST['credit_card'];
+    if (isset($credit_card)) {
+        echo $credit_card;
+    } else {
+        echo "Please select a credit card";
+    }
+}
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
-
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
-
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
+// checkbox
+if (!empty($_POST)) {
+    if (isset($_POST["pizza"])) {
+        echo $_POST["pizza"];
+    } if (isset($_POST["pasta"])) {
+        echo $_POST["pasta"];
+    } if (isset($_POST["salad"])) {
+        echo $_POST["salad"];
+    }
+}
 ?>
