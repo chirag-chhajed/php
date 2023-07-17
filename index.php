0ad151f087e20a6f7eaf735fbd9155fa2dc74ac1
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 22:52:37 +0530

    cookit added

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
-    // Q: Explain include() in php
-    // A: include() is used to include a php file into another php file.
-    //    If the file is not found, it will throw a warning and continue the execution of the script.
-    //    If the file is found, it will include the file and continue the execution of the script.
+// Q: Explain include() in php
+// A: include() is used to include a php file into another php file.
+//    If the file is not found, it will throw a warning and continue the execution of the script.
+//    If the file is found, it will include the file and continue the execution of the script.
+setcookie("fav_food", "pasta", time() + (86400 * 2), "/");
+include("header.html")
 
-    include("header.html")
-    
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
+
 <body>
     <p>This is the Home page</p>
     <p>Stuff about your home page can go here</p>
 </body>
+
 </html>
 <?php
-    include("footer.html");
+include("footer.html");
