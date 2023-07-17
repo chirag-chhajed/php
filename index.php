46c81a8073a63e41677c629fefe308c1acab1bb1
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 13:44:31 +0530

    echo and comments done

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Hello World<br>";
+    echo "I am learning php.";
+    // This is a comment
+    /* This is a
+    multi line comment */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
