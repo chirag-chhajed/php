d58f781516dec9b2d0110b20a08119bdc6171007
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 22:23:00 +0530

    done

--- index.php
+++ index.php
@@ -1,36 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        Username: <br>
+        <input type="text" name="Username" id=""><br>
+        age:<br>
+        <input type="text" name="age" id=""><br>
+        email:<br>
+        <input type="email" name="email" id=""><br>
+        <input type="submit" value="login" name="login">
+    </form>
+</body>
+
+</html>
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+if (isset($_POST['login'])) {
+    $username = filter_input(INPUT_POST, 'Username', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+    if(empty($age)){
+        echo "That number wasn't valid";
+    } else{
+        echo "Your age is $age";
+    }
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
-
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
-?>
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if(empty($email)){
+        echo "That email wasn't valid";
+    } else{
+        echo "Your email is $email";
+    }
+    // echo "Welcome $username <br> your age is $age<br> your email is $email";
+}
