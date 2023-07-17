c983d553cc6e8b7308a9a558b5f92b2d3499fb0c
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 19:17:59 +0530

    done

--- index.php
+++ index.php
@@ -1,36 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="">Enter a country</label>
+        <input type="text" name="country" id="">
+        <input type="submit" >
+    </form>
+</body>
+</html>
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+// associative array
+// key => value
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+$capitals = array(
+    "USA" => "Washington DC",
+    "Japan" => "Tokyo",
+    "South Korea" => "Seoul",
+    "China" => "Beijing",
+    "Indea" => "New Delhi"
+);
+// echo $capitals["USA"];
+// $capitals["France"] = "Paris";
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
+// Q:What does array_pop do?
+// A:Removes the last element from an array
+// Q:What does array_shift do?
+// A:Removes the first element from an array
+// @: What does array_reverse do?
+// A: Reverses the order of the elements in an array
 
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
+// for iterating over key and value both
+foreach($capitals as $key => $value){
+    echo "$key $value <br>";
+}
 
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
+// for iterating over keys
+foreach ($capitals as $value) {
+    echo "{$value} <br>";
+}
 
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
+$capital = $capitals[$_POST["country"]];
 
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
-?>
+echo "The capital is $capital";
