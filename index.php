438e7867b5e0e6c724fad99263a15270b738d4ab
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 18:18:28 +0530

    array done

--- index.php
+++ index.php
@@ -1,36 +1,16 @@
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+    $foods = array("apple","orange","banana","grapes");
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+    echo $foods[0] . "<br>";
+    echo $foods[1] . "<br>";
+    echo $foods[2] . "<br>";
+    echo $foods[3] . "<br>";
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
+    $foods[4] = "pineapple";
+    array_push($foods,"kiwi");
+    $reversed_foods = array_reverse($foods);
 
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
-
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
-?>
+    foreach($reversed_foods as $food){
+        echo $food . "<br>";
+    }
+    echo count($foods);
