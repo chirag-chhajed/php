a99c9b2bda4ae8147293acd0c1e574c1bc58da9c
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-17 13:58:32 +0530

    lesson 2 done

--- index.php
+++ index.php
@@ -1,19 +1,36 @@
 <?php
-    echo "Hello World<br>";
-    echo "I am learning php.";
-    // This is a comment
-    /* This is a
-    multi line comment */
+    // string
+    $name = "Chirag";
+    $food = "pizza";
+    $email = "fake@email.com";
+
+    // integers
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    // float
+    $gpa = 2.5;
+    $price = 4.99;
+    $tax_rate = 5.1;
+
+    // boolean
+    $employed = true;
+    $online = false;
+    $for_sale = true;
+
+    echo "Hello, {$name}<br>";
+    echo "You like {$food}<br>";
+    echo "Your email is {$email}<br>";
+
+    echo "You are {$age} years old<br>";
+    echo "There have {$users} users online<br>";
+    echo "You would like to buy {$quantity} items<br>";
+
+    echo "Your gpa is: {$gpa}<br>";
+    echo "Your pizza is \${$price}<br>";
+    echo "The sales is tax rate is {$tax_rate}%<br>";
+
+    echo "Are you employed? {$employed}<br>";
+    echo "Are you online? {$online}<br>";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <br>
-    <button>order pizza</button>
-</body>
-</html>
