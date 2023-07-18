ad872bcd948bcabe3d859f27de37aae32e3c1b9d
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-07-18 10:09:21 +0530

    db done

--- index.php
+++ index.php
@@ -1,36 +1,40 @@
 <?php
-    // string
-    $name = "Chirag";
-    $food = "pizza";
-    $email = "fake@email.com";
+// 1. Mysqli extension
+// 2. pdo (php data objects)
 
-    // integers
-    $age = 21;
-    $users = 2;
-    $quantity = 3;
+include("database.php");
 
-    // float
-    $gpa = 2.5;
-    $price = 4.99;
-    $tax_rate = 5.1;
+// $username = "mr_crabs";
+// $password = "123456";
+// $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    // boolean
-    $employed = true;
-    $online = false;
-    $for_sale = true;
+$sql = "SELECT * FROM users";
 
-    echo "Hello, {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $row["id"] . "<br>";
+        echo $row["user_name"] . "<br>";
+        echo $row["reg_date"] . "<br>";
+    }
+} else {
+    echo "No user found";
+}
+// echo "User is now registerd";
 
-    echo "You are {$age} years old<br>";
-    echo "There have {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price}<br>";
-    echo "The sales is tax rate is {$tax_rate}%<br>";
-
-    echo "Are you employed? {$employed}<br>";
-    echo "Are you online? {$online}<br>";
+mysqli_close($conn);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
